b49750135fc838a75bac804c3090dfe15112c392
Алена Карпова <alenapancenko00@gmail.com>
2022-10-25 15:39:19 +0300

    .init

---
+++ Movie/Movie/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Алена Панченко on 25.10.2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
